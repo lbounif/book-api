8600525088828c799cbadd3a0bbeeb9ceebb9442
lynda <ahlem@gmail.com>
2021-08-09 09:51:13 +0100

    add bookRouter and db in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
+//Creation of server in Nodejs
 //1. Import express
 const express = require("express")
+require("./db/mongoose")
+const bookRouter = require("./routes/bookRouter")
 
 //2. Call express function and save it in app
 const app = express()
 
 app.use(express.json())
+app.use(bookRouter)
 
 //3. Define a port
 const port = process.env.PORT || 10010
