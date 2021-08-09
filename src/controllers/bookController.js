6084d87dd9dd1e956a73509040a30c3efb989308
lynda <ahlem@gmail.com>
2021-08-09 09:38:49 +0100

    init book controller

---
+++ src/controllers/bookController.js
@@ -0,0 +1,23 @@
+const addBook = async(req, res) => {
+
+}
+const getBooks = async(req, res) => {
+    
+}
+const getBookById = async(req, res) => {
+    
+}
+const updateBook = async(req, res) => {
+    
+}
+const deleteBook = async(req, res) => {
+    
+}
+
+module.exports = {
+    addBook,
+    getBooks,
+    getBookById,
+    updateBook,
+    deleteBook
+}
